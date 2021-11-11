c46c54810690d209b520be26a7656bbabfbcae9e
Ivy Li <ivyli6477@gmail.com>
2021-11-11 17:10:21 -0500

    5

--- hw7/cipher_xl3110/tests/test_cipher_xl3110.py
+++ hw7/cipher_xl3110/tests/test_cipher_xl3110.py
@@ -1,1 +1,8 @@
 from cipher_xl3110 import cipher_xl3110
+
+import pytest
+
+def test_single():
+    actual = cipher_xl3110.cipher(text = "ivy", shift = 2)
+    expected = "kxA"
+    assert actual == expected, "Should give kxA"
